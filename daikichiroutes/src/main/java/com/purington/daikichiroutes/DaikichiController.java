0547db188a027f4388c8d24f46df99091d475e29
PPurington77 <papurington@gmail.com>
2022-08-04 20:03:32 -0600

    update
    completed daikichi core

--- daikichiroutes/src/main/java/com/purington/daikichiroutes/DaikichiController.java
+++ daikichiroutes/src/main/java/com/purington/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.purington.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,20 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow") 
 	public String tunefor() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{location}")
+	public String travel(@PathVariable("location") String location){
+		return "Congratulations! You will soon travel to " + location;
+	}
+	
+	@RequestMapping("/lotto/{number}") 
+	public String lotto(@PathVariable("number") int number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
 	}
 }
